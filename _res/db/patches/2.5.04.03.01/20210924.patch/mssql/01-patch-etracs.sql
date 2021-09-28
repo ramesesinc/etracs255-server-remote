23856eb24d472fc163b06c63dd11c9a6c74c685a
worgflores <worgflores@gmail.com>
2021-09-28 09:37:15 +0800

    _res: patches: updated

---
+++ _res/db/patches/2.5.04.03.01/20210924.patch/mssql/01-patch-etracs.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO sys_var (name, value, description, datatype, category) 
+VALUES ('CASHBOOK_CERTIFIED_BY_NAME', NULL, 'Cashbook Report Certified By Name', 'text', 'REPORT');
+
+INSERT INTO sys_var (name, value, description, datatype, category) 
+VALUES ('CASHBOOK_CERTIFIED_BY_TITLE', NULL, 'Cashbook Report Certified By Title', 'text', 'REPORT');
